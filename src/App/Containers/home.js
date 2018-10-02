2d65b034a3ed1929b0151529285bab28f4c32e13
Enrique Devars <e.devars.dev@gmail.com>
2018-10-02 13:05:25 -0500

    Adding a switch structure to control the path of each card image

--- src/App/Containers/home.js
+++ src/App/Containers/home.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import Card from '../Components/card'
 import AppLyaout from '../Components/AppLayout';
+
 class Home extends Component{
     constructor(){
         super();
@@ -23,7 +24,8 @@ class Home extends Component{
                     health: sign.salud, 
                     money: sign.dinero, 
                     color: sign.color, 
-                    luckyNumber: sign.numero
+                    luckyNumber: sign.numero,
+                    url: '../../img/'+sign.nombre+'.jpg' 
                 }
                 this.setState({sunSigns: this.state.sunSigns.concat([data])})
             }
@@ -34,12 +36,57 @@ class Home extends Component{
             <AppLyaout>
                 {
                     this.state.sunSigns.map((signo)=>{
+                        var image
+                        switch (signo.name) {
+                            case 'Acuario':
+                                image=require('../../img/Acuario.jpg')
+                                break;
+                            case 'Aries':
+                                image=require('../../img/Aries.jpg')
+                                break;
+                            case 'Cáncer':
+                                image=require('../../img/Cáncer.jpg')
+                                break;
+                            case 'Capricornio':
+                                image=require('../../img/Capricornio.jpg')
+                                break;
+                            case 'Escorpión':
+                                image=require('../../img/Escorpión.jpg')
+                                break;
+                            case 'Géminis':
+                                image=require('../../img/Géminis.jpg')
+                                break;
+                            case 'Leo':
+                                image=require('../../img/Leo.jpg')
+                                break;
+                            case 'Libra':
+                                image=require('../../img/Libra.jpg')
+                                break;
+                            case 'Piscis':
+                                image=require('../../img/Piscis.jpg')
+                                break;
+                            case 'Sagitario':
+                                image=require('../../img/Sagitario.jpg')
+                                break;
+                            case 'Tauro':
+                                image=require('../../img/Tauro.jpg')
+                                break;
+                            case 'Virgo':
+                                image=require('../../img/Virgo.jpg')
+                                break;
+                        }
                         return(
                             <Card 
                                 key={signo.name}
                                 title={signo.name}
                                 content={signo.love}
-                                url='https://storage.googleapis.com/mmc-elcaribe-bucket/uploads/2018/09/4df34464-horoscopo-17-abril-t.jpg'
+                                signDate={signo.signDate}
+                                love={signo.love}
+                                health={signo.love}
+                                money={signo.money}
+                                color={signo.color}
+                                luckyNumber={signo.luckyNumber}
+                                url={image}
                             />
                         )
                     })
